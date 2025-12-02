96d15a3c5fae119e2da3241544ca6ecac8ae771d
HaydenH208 <johncbtdaw@gmail.com>
2025-12-02 08:57:20 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,51 +1,54 @@
-
 <?php
-// Initialize variables for the result message and the input text.
-$checkResult = "<span style='color:red'> Try the magic word 'fred'</span>";
-$myInputText01 = '';
+// Default result message
+$resultMessage = "<span style='color:blue'>Enter a number to analyze it!</span>";
+$userNumber = "";
 
-// Check if the form was submitted (i.e., if the request method is POST)
+// Handle POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Get the input text from the form data.
-    // We use isset() and the null coalescing operator (??) for safety.
-    $myInputText01 = $_POST['myText01'] ?? '';
 
-    // The logic from the Node.js application
-    $myCheck = false;
-    if ($myInputText01 === 'fred') {
-        $myCheck = true;
-    }
+    // Get user input
+    $userNumber = $_POST['myNumber'] ?? '';
 
-    // Determine the result message and color
-    if ($myCheck) {
-        $checkResult = "<b style='color:green'> Cool! </b>";
+    // Try converting input to a number
+    if (is_numeric($userNumber)) {
+
+        $n = floatval($userNumber);
+        $square = $n * $n;
+        $cube   = $n * $n * $n;
+        $evenOdd = ((int)$n % 2 === 0) ? "even" : "odd";
+
+        $resultMessage = "
+        <div style='color:green; margin-top:15px;'>
+            <b>Results for $n:</b><br>
+            Square: $square <br>
+            Cube: $cube <br>
+            Even/Odd: $evenOdd
+        </div>
+        ";
+
     } else {
-        $checkResult = "<span style='color:red'> Try the magic word 'fred'</span>";
+        $resultMessage = "<span style='color:red'>Please enter a valid number!</span>";
     }
 }
-
-// Now, render the HTML page, embedding the dynamic content.
-// The form's 'action' is set to the current file (index.php) and the method is 'post'.
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Browser Title</title>
+    <title>PHP Number Analyzer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <h3>Render-PHP-submit</h3>
 
-    <form action="" method="post" >
-        <label for="myText01">Enter Text:</label>
-        <!-- The 'value' attribute keeps the entered text after submission -->
-   <!-- <input type="text" id="myText01" name="myText01" value="<?= htmlspecialchars($myInputText01); ?>"> -->
-        <input type="text" id="myText01" name="myText01" value="">
-        <input type="submit" value="Submit">
+    <h3>PHP Number Analyzer</h3>
+
+    <form action="" method="post">
+        <label for="myNumber">Enter a number:</label>
+        <input type="text" id="myNumber" name="myNumber" value="">
+        <input type="submit" value="Analyze">
     </form>
-    
-    <div style="text-align: center;">
-        <?php echo $checkResult; ?>
+
+    <div style="text-align:center; margin-top:20px;">
+        <?= $resultMessage ?>
     </div>
 
 </body>
